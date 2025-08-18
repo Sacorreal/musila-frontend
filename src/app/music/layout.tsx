3f3abb91f31980260efc1a3a0cdae39c57dc7543
Francisco <franciscojdalessandro@gmail.com>
2025-08-18 12:04:25 +0200

    Feature/audio/player

--- src/app/music/layout.tsx
+++ src/app/music/layout.tsx
@@ -4,34 +4,41 @@ import React, { useEffect } from "react";
 import { useSidebar } from "@/shared/hooks/useSidebar";
 import Sidebar from "@/shared/components/Layout/Sidebar";
 import ContentApp from "@/shared/components/Layout/ContentApp";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+import { mockTracks } from "./Mocks/tracks";
 
 export default function MusicAppLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  const { isLoggedIn, isSidebarOpen, setIsSidebarOpen } = useSidebar();
+    const { isLoggedIn, isSidebarOpen, setIsSidebarOpen } = useSidebar();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => {
+    useEffect(() => {
+        if (!isLoggedIn) {
+            router.replace("/login");
+        }
+    }, [isLoggedIn, router]);
+
     if (!isLoggedIn) {
-      router.replace("/login");
+        return null;
     }
-  }, [isLoggedIn, router]);
 
-  if (!isLoggedIn) {
-    return null;
-  }
+    const AudioPlayer = dynamic(() => import("@/app/music/components/AudioPlayer"), { ssr: false });
 
-  return (
-    <div className="flex min-h-screen bg-background">
-      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+    return (
+        <div className="flex min-h-screen bg-background">
+            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
 
-      <ContentApp onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}>
-        {children}
-      </ContentApp>
-    </div>
-  );
+            <ContentApp onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}>
+                {children}
+                <div className="fixed left-0 md:left-64 right-0 bottom-0 px-4 py-3 z-30">
+                    <AudioPlayer initialQueue={mockTracks} />
+                </div>
+            </ContentApp>
+        </div>
+    );
 }
